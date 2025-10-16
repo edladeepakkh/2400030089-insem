a690ce6bf037fa6cab3a117aa4574a2606377555
EDLA DEEPAKKH <2400030089@kluniversity.in>
2025-10-16 20:32:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+// Functional component that receives props
+function Welcome(props) {
+  return <h1>Welcome, {props.name}!</h1>;
+}
+
+// Example usage of the Welcome component with different names
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome name="Deepak" />
+      <Welcome name="Sarah" />
+      <Welcome name="John" />
     </div>
   );
 }
